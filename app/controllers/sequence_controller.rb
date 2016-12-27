ea33bc145f1a6be4094561f823014a1963729942
Traci7822 <Tracir@gmail.com>
2016-12-27 06:28:39 -0700

    adding poses properly to sequence

--- app/controllers/sequence_controller.rb
+++ app/controllers/sequence_controller.rb
@@ -9,18 +9,18 @@ class SequenceController < ApplicationController
   end
 
   def create
-    binding.pry
     @sequence = Sequence.create(title: sequence_params[:title], difficulty: sequence_params[:difficulty], repititions: sequence_params[:repititions])
     if @sequence.valid?
       # build method to add poses here (pre-existing & new)
       @sequence.set_sequence_poses(sequence_params)
+    else
+      render :index
     end
   end
 
   private
 
   def sequence_params
-    # new poses aren't passing in input
     params.require(:sequence).permit(:title, :difficulty, :repititions, poses_attributes: [:sanskrit_name, :english_name], pose_ids: [])
   end
 end
--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -14,5 +14,30 @@ class Sequence < ActiveRecord::Base
       end
     end
   end
-  
+
+  def set_sequence_poses(params)
+    @sequence_poses_array = []
+    params.each do |attribute|
+      if attribute[0] == "pose_ids"
+        attribute[1].each_with_index do |pose_id, i|
+          if pose_id != ""
+            if pose_id == "new"
+              @new_pose_names = params[:poses_attributes].values[i]
+              if @new_pose_names.values[0] != "" || @new_pose_names.values[1] != ""
+                @new_pose = Pose.find_or_create_by(sanskrit_name: @new_pose_names[:sanskrit_name], english_name: @new_pose_names[:english_name])
+                @new_pose.save
+                @sequence_poses_array[i] = @new_pose
+              else
+                # throw error, names cannot be blank
+              end
+            else
+              @sequence_poses_array[i] = Pose.find(pose_id)
+            end
+          end
+        end
+      end
+    end
+  end
+
+
 end
