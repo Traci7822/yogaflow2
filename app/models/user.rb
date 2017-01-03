0dc120eb212f3b030b662b449fa92ab4b2d9374b
Traci7822 <Tracir@gmail.com>
2017-01-03 15:40:57 -0700

    validating password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
+  validates_confirmation_of :password
+
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create.tap do |user|
       user.provider = auth.provider
