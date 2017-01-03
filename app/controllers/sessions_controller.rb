a9a40aa88e78c8ec5de03a1bca43aca94d86eaca
Traci7822 <Tracir@gmail.com>
2017-01-03 15:50:20 -0700

    built create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,27 @@
 class SessionsController < ApplicationController
+  include Sessionable
+  
+  def new
+    @user = User.new
+  end
+
   def create
+  @user = User.find_by(username: params[:username])
+  if @user && @user.authenticate(params[:password])
+    set_session
+    redirect_to root_path
+  elsif
+    @user.nil?
+    @user = User.new
+    @user.errors[:username] << "was not found"
+    render :new
+  elsif
+    @user.errors[:password] << "is incorrect"
+    render :new
+  end
+end
+
+  def create_with_facebook
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path
