003693534d99334e622d0cdc5eed7d23d476734f
Traci7822 <Tracir@gmail.com>
2017-01-04 13:10:45 -0700

    added username and email validity and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :username, presence: true, length: {minimum: 5}, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates_confirmation_of :password
   require 'securerandom'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  
+describe User do
+  context "signs up for an account" do
+
+    describe "signs up with a valid username" do
+      it "is not blank" do
+        User.create(username: nil, email: 'Bob@BobTheDog.com', password: 'woofwoof').should_not be_valid
+      end
+      it "is long enough" do
+        User.create(username: 'Bob', email: 'Bob@BobTheDog.com', password: 'woofwoof').should_not be_valid
+      end
+      it "is unique" do
+        User.create(username: 'BobTheDog', email: 'Bob@BobTheDog.com', password: 'woofwoof').should be_valid
+        User.create(username: 'BobTheDog', email: 'Bob@BobTheDog.com', password: 'woofwoof').should_not be_valid
+      end
+    end
+
+    describe "signs up with a valid email" do
+      it "is not blank" do
+        User.create(username: 'BobTheDog', email: nil, password: 'woofwoof').should_not be_valid
+      end
+      it "is a valid email address" do
+        User.create(username: 'BobTheDog', email: 'Bob_at_BobTheDog.com', password: 'woofwoof').should_not be_valid
+        User.create(username: 'BobTheDog', email: 'Bob@BobTheDog,com', password: 'woofwoof').should_not be_valid
+      end
+      it "is unique" do
+        User.create(username: 'BobTheDog', email: 'Bob@BobTheDog.com', password: 'woofwoof').should be_valid
+        User.create(username: 'BobTheDog', email: 'Bob@BobTheDog.com', password: 'woofwoof').should_not be_valid
+      end
+    end
+  end
 end
