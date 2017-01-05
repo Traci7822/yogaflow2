f44f8d5b607c18772c59c541c369b2dcabe77e92
Traci7822 <Tracir@gmail.com>
2017-01-04 18:55:30 -0700

    adjusted validation methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true, length: {minimum: 5}, uniqueness: true
+  validates :username, presence: true, length: {minimum: 5}, uniqueness: true, :on => :create
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :password, presence: true, length: {minimum: 6}
-  validates_confirmation_of :password
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, :on => :create
+  validates :password, presence: true, length: {minimum: 6}, :on => :create
+  validates_confirmation_of :password, :on => :create
   require 'securerandom'
 
   def self.from_omniauth(auth)
