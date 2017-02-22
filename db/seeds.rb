962f5197e1175aa7d9d0a2603e7d6af8224bd861
Traci7822 <Tracir@gmail.com>
2017-02-22 11:53:10 -0700

    changed db spec for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-sequence_list = [
-  ["Surya Namaskar A", 2, 1, [1, 2, 4, 2, 1]],
-  ["Standing Balance", 3, 2, [9, 11, 12, 7, 16]],
-  ["Half Mandala Surrender", 3, 3, [5, 9, 6, 10, 3]],
-  ["Energizing Solar Sequence", 2, 5, [3, 7, 12, 1, 2, 15, 17]]
-]
 
-sequence_list.each do |title, difficulty, repititions, pose_ids|
-  Sequence.create(title: title, difficulty: difficulty, repititions: repititions, pose_ids: pose_ids)
-end
 
 
 pose_list = [
@@ -39,4 +30,15 @@ pose_list = [
 
 pose_list.each do |sanskrit_name, english_name|
   Pose.create(sanskrit_name: sanskrit_name, english_name: english_name)
+end
+
+sequence_list = [
+  ["Surya Namaskar A", 2, 1, [1, 2, 4, 2, 1]],
+  ["Standing Balance", 3, 2, [9, 10, 8, 7, 1]],
+  ["Half Mandala Surrender", 3, 3, [5, 9, 6, 10, 3]],
+  ["Energizing Solar Sequence", 2, 5, [3, 7, 9, 1, 2, 10, 4]]
+]
+
+sequence_list.each do |title, difficulty, repititions, pose_ids|
+  Sequence.create(title: title, difficulty: difficulty, repititions: repititions, pose_ids: pose_ids)
 end
